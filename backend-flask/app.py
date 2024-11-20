438a8bbe12ea58154d51d5e46900c1f8404b655d
Guillermo Escutia <escutia_90@hotmail.com>
2024-11-20 21:13:19 +0000

    instrument xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,6 +22,9 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#AWS xray imports
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -29,6 +32,11 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+#xray 
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 # Initialize automatic instrumentation with Flask
